73b86eab2610cbfd614b8c43e202703dbd5e8298
Jason Goodwin <jay.michael.goodwin@gmail.com>
2018-01-05 16:22:13 -0500

    install all of the ido things

--- init.el
+++ init.el
@@ -1,3 +1,28 @@
+(require 'package)
+(add-to-list
+ 'package-archives
+ ;; '("melpa" . "http://stable.melpa.org/packages/") ; many packages won't show if using stable
+ '("melpa" . "http://melpa.milkbox.net/packages/")
+ t)
+(package-initialize)
+
+;; ido things
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+(require 'ido-completing-read+)
+(ido-ubiquitous-mode 1)
+
+(require 'smex) ; Not needed if you use package.el
+(smex-initialize) ; Can be omitted. This might cause a (minimal) delay
+                  ; when Smex is auto-initialized on its first run.
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+;; key bindings
 (global-unset-key (kbd "<left>"))
 (global-unset-key (kbd "<right>"))
 (global-unset-key (kbd "<up>"))
@@ -15,4 +40,17 @@
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
 
+;; save when navigating away
 (add-hook 'focus-out-hook (lambda () (save-some-buffers t)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (darcula-theme smex ido-completing-read+))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
