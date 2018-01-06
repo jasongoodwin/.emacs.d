803b1a02653afc06c6088b114939f005ca98d98b
Jason Goodwin <jay.michael.goodwin@gmail.com>
2018-01-06 14:17:31 -0500

    add nyan mode

--- init.el
+++ init.el
@@ -91,13 +91,16 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (elixir-mode helm-projectile helm projectile darcula-theme smex ido-completing-read+))))
+    (nyan-mode elixir-mode helm-projectile helm projectile darcula-theme smex ido-completing-read+))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
+ )
 (projectile-global-mode)
 (setq projectile-completion-system 'helm)
 (helm-projectile-on)
+(nyan-mode)
+(nyan-toggle-wavy-trail)
+(nyan-start-animation)
